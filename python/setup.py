e76423ee34b7a191b66f1b2d753050bb514248b0
Jonas Tolkemitt <jonas.tolkemitt@gmail.com>
2023-07-24 19:46:50 +0200

    Add setup file

---
+++ python/setup.py
@@ -0,0 +1,52 @@
+"""
+MIT License
+
+Copyright (c) 2023 YeetlePrime
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+
+README = ""
+with open("readme.md") as file:
+    README = file.read()
+
+
+classifiers = [
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='ch.py',
+  version=VERSION,
+  description='A python-wrapper for the unofficial Clicker Heroes API.',
+  long_description=README,
+  url='https://github.com/YeetlePrime/UnofficialClickerHeroesAPI',  
+  author='YeetlePrime',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Clicker Heroes API wrapper', 
+  packages=find_packages(),
+  install_requires=[] 
+)
